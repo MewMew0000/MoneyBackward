153e11c236319e71087903245d72b2f5736cbef2
MewMew <charleeeeswangj@gmail.com>
2024-10-12 10:46:51 +0900

    Docker Composeを使用してアプリケーションを一括で起動および停止する。

--- conf/mysql_conf.go
+++ conf/mysql_conf.go
@@ -4,12 +4,12 @@ type Mysql struct {
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
 	Config   string `yaml:"config"`
-	Conn     string `yaml:"conn"`
+	DefaultSchema    string `yaml:"defaultSchema"`
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
 	LogLevel string `yaml:"logLevel"`
 }
 
 func (m Mysql) Dsn() string {
-	return m.Username + ":" + m.Password + "@tcp(" + m.Host + ":" + m.Port + ")/" + m.Conn + "?" + m.Config
+	return m.Username + ":" + m.Password + "@tcp(" + m.Host + ":" + m.Port + ")/" + "?" + m.Config
 }
---
+++ start.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+docker rmi mew_server:0.0.1
+docker build -t mew_server:0.0.1 .
+docker-compose up -d
+docker-compose ps
